2663c4eea964d3e04b22e790e28fd3f5da31a30f
gutovesco <guto.vesco@hotmail.com>
2021-01-19 16:40:13 -0300

    created the basic layout of the page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import React from 'react';
+import { Details } from './components/Details/Details'
+import { Main } from './components/Main/Main'
+import { Grid } from '@material-ui/core';
+import useStyles from './styles';
 
-function App() {
+export default function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <h1>hello app</h1>
-    </div>
-  );
+    <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{ height: '100vh' }}>
+      <Grid item xs={12} sm={4}>
+        <Details title="Income" />
+      </Grid>
+      <Grid item xs={12} sm={3}>
+        <Main />
+      </Grid>
+      <Grid item xs={12} sm={4}>
+        <Details title="Expense" />
+      </Grid>
+    </Grid>
+  )
 }
-
-export default App;
